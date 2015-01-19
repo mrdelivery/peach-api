91341cd4a4bd67ad7d7a444eb781a4bccf678ffe
Michael Nel <michael.nel@mrd.com>
2015-01-20 01:01:25 +0200

    Some docs

--- src/Commands/CommandHandlerTransalator.php
+++ src/Commands/CommandHandlerTransalator.php
@@ -1,5 +1,15 @@
 <?php namespace Mnel\Peach\Commands;
 
+/**
+ * The CommandHandlerTranslator recieves a Command instance and
+ * produces it's corresponding CommandHandler counter-part.
+ *
+ * The default implementation is the ArrayCommandHandlerTranslator.
+ * It recieves an array of CommandHandler instances, indexed by
+ * the Full Domain Name of the corresponding Command class.
+ *
+ * @package Mnel\Peach\Commands
+ */
 interface CommandHandlerTransalator
 {
     /**
