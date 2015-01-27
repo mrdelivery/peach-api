89580af7c6250708491fe60a1d3943556a1e9fd4
Michael Nel <michael.nel@mrd.com>
2015-01-27 16:46:47 +0200

    Made command exception more generic in order for entire namepsace to be able to utilise it

--- src/Commands/CommandException.php
+++ src/Commands/CommandException.php
@@ -3,22 +3,4 @@
 use Exception;
 use Mnel\Peach\PeachException;
 
-class CommandException extends PeachException
-{
-    /** @var Command */
-    private $command;
-
-    public function __construct(Command $command, $message = "", $code = 0, Exception $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-        $this->command = $command;
-    }
-
-    /**
-     * @return Command
-     */
-    public function getCommand()
-    {
-        return $this->command;
-    }
-}
+class CommandException extends PeachException {}
